297b048aa1a370649a374f97c9cc2fd97b428b29
Yogeshwar Velingker <yvelingk@mit.edu>
2020-05-28 18:48:45 -0400

    Add gradient function

--- maxcut.py
+++ maxcut.py
@@ -2,6 +2,10 @@ import numpy as np
 import networkx as nx
 #import matplotlib.pyplot as plt
 
+def x_gate(state, i):
+    reshaped = state.reshape(state.size >> (i + 1), 1 << (i + 1))
+    return np.roll(reshaped, 1 << i, axis=1).ravel()
+
 class MaxCut:
     def __init__(self, graph):
         self.qubits = len(graph)
@@ -27,24 +31,24 @@ class MaxCut:
         c = np.cos(angle)
         s = 1j*np.sin(angle)
         for i in range(self.qubits):
-            # The next 3 lines apply X to the ith qubit
-            swapped = state.copy()
-            swapped = swapped.reshape(state.size >> (i + 1), 1 << (i + 1))
-            swapped = np.roll(swapped, 1 << i, axis=1).ravel()
+            xstate = x_gate(state, i)
             state *= c
-            state -= s*swapped
+            state -= s*xstate
 
     def evolve_hc(self, state, angle):
         state *= np.exp(-1j*angle*self.hc)
 
-    def prepare_state(self, angles, init_state=None):
+    def prepare_state(self, angles, init_state=None, starthb=False):
         if init_state is None:
-            state = np.full(2**self.qubits, np.exp2(-self.qubits/2), dtype=complex)
-        for gamma, beta in zip(*[iter(angles)]*2):
-            self.evolve_hc(state, gamma)
-            self.evolve_hb(state, beta)
-        if len(angles) % 2 == 1:
-            self.evolve_hc(state, angles[-1])
+            state = np.full(len(self.hc), np.exp2(-self.qubits/2), dtype=complex)
+        else:
+            state = init_state.copy()
+        for i in angles:
+            if starthb:
+                self.evolve_hb(state, i)
+            else:
+                self.evolve_hc(state, i)
+            starthb = not starthb
         return state
 
     def objective_state(self, state, exact=True, n=1):
@@ -59,3 +63,31 @@ class MaxCut:
     def objective(self, angles, exact=True, n=1):
         state = self.prepare_state(angles)
         return self.objective_state(state, exact, n)
+
+    def gradient(self, angles, indices, return_obj=False):
+        ret = np.empty(len(indices))
+        idx, inv = np.unique(indices, return_inverse=True)
+        idx = np.concatenate(([0], idx))
+        inv += 1
+        left = [None]*len(idx)
+        derivs = np.empty(len(idx))
+        for i in range(1, len(idx)):
+            left[i] = self.prepare_state(angles[idx[i-1]:idx[i]], left[i-1], idx[i-1])
+        left[0] = self.prepare_state(angles[idx[-1]:], left[-1], idx[-1])
+        right = self.hc*left[0]
+        for i in range(1, len(idx)):
+            if idx[i] % 2 == 0:
+                state = np.zeros_like(left[i])
+                for j in range(self.qubits):
+                    state += x_gate(left[i], j)
+                left[i] = state
+            else:
+                left[i] *= self.hc
+            left[i] = self.prepare_state(angles[idx[i]:], left[i], idx[i])
+            derivs[i] = 2*np.imag(np.vdot(left[i], right))
+        for i in range(len(ret)):
+            ret[i] = derivs[inv[i]]
+        if return_obj:
+            return ret, -np.real(np.vdot(left[0], right))
+        else:
+            return ret
